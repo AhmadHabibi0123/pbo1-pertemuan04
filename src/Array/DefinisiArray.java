3b216d143cc440729d871559efad225f8fb6395a
AhmadHabibi0123 <habibiahmad0123@gmail.com>
2020-04-25 12:12:25 +0700

    Cara Lain Mendefinisikan Array

---
+++ src/Array/DefinisiArray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class DefinisiArray {
+    public static void main(String[] args) {
+        String matakuliah[];
+        matakuliah = new String[10];
+
+        matakuliah[0] = "Pemrgram Berbasis Objek 1";
+        matakuliah[1] = "Visul 2";
+        matakuliah[2] = "Rekayasa Perangkat Lunak";
+        matakuliah[3] = "Organisasi Komputer";
+        matakuliah[4] = "Pemrograman Web 2";
+
+    }
+}
